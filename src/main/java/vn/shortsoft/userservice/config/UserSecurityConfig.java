6df0e0a5a61bbb0308c2d3d6a73cc500e2bf3bdd
CEO-VT3 <taint380@gmail.com>
2024-06-13 19:16:40 +0700

    update 13-6-2024

--- src/main/java/vn/shortsoft/userservice/config/UserSecurityConfig.java
+++ src/main/java/vn/shortsoft/userservice/config/UserSecurityConfig.java
@@ -1,30 +1,63 @@
 package vn.shortsoft.userservice.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class UserSecurityConfig {
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private String secretKey = "24q5PhwxA02MndFFZ9HZmeBQ2w54wU7TvQgux189O0gjqizOeSbLBnGcFsXvPqxx";
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, "/v1/user/*")
                 .permitAll()
                 .anyRequest()
-                .authenticated());
+                .authenticated())
+                .httpBasic(Customizer.withDefaults());
+        httpSecurity.oauth2Login(Customizer.withDefaults()); 
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), MacAlgorithm.HS512.getName());
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/vn/shortsoft/userservice/controller/UserController.java
+++ src/main/java/vn/shortsoft/userservice/controller/UserController.java
@@ -2,6 +2,7 @@ package vn.shortsoft.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import vn.shortsoft.userservice.dto.UserDto;
 import vn.shortsoft.userservice.model.User;
 import vn.shortsoft.userservice.response.DataResponse;
 import vn.shortsoft.userservice.service.UserService;
+
 
 @RestController
 @RequestMapping("/v1/user")
@@ -29,4 +31,9 @@ public class UserController {
         return ResponseEntity.ok().body(userService.verifyLoginByPassword(userDto));
 
     }
+    @GetMapping("/hello")
+    public String getMethodName() {
+        return "Helloworld";
+    }
+    
 }
